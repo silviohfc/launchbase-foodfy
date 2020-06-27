3337d478a8b57611cd6273e18ef5b10f8b1f425d
Silvio Henrique <silviohfc@gmail.com>
2020-06-27 14:47:39 -0300

    Created routes and template with nunjucks

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 
 const server = express()
 
+server.use(express.static('public'))
 
+server.set("view engine", "njk")
 
+nunjucks.configure("views", {
+    express: server
+})
+
+// ROUTES
 server.get("/", (req, res) => {
-    return res.send("Hello world!")
+    return res.render("index")
+})
+
+server.get("/about", (req, res) => {
+    return res.render("about")
+})
+
+server.get("/recipes", (req, res) => {
+    return res.render("recipes")
 })
 
 
