171bb3cf70d97f83bfe1340f8e5bcb6d89c50034
Silvio Henrique <silviohfc@gmail.com>
2020-10-06 15:16:33 -0300

    Saving data to JSON file

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,8 @@
-const data = require('../data')
+const fs = require('fs')
+const data = require('../data.json')
 
 exports.index = (req, res) => {
-    return res.render("admin/index", { recipes: data })
+    return res.render("admin/index", { recipes: data.recipes })
 }
 
 exports.create = (req, res) => {
@@ -9,9 +10,30 @@ exports.create = (req, res) => {
 }
 
 exports.post = (req, res) => {
-    return res.send(req.body)
+    let { image, title, author, ingredients, steps, information } = req.body
+
+    data.recipes.push({
+        image,
+        title,
+        author,
+        ingredients,
+        steps,
+        information
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), err => {
+        if (err) return res.send("Write file error!")
+
+        return res.redirect("/admin/recipes")
+    })
 }
 
 exports.show = (req, res) => {
-    return res.render("admin/recipe_info", { recipe: data[0] })
+    const recipeId = req.params.id
+    
+    if(!data.recipes[recipeId - 1]) {
+        res.send("A receita não existe")
+    } else {
+        res.render("admin/recipe_info", { recipe: data.recipes[recipeId - 1] })
+    }
 }
