d6d8b818303a59a06de859b85dddcb886a7c0fe1
Silvio Henrique <silviohfc@gmail.com>
2020-10-19 13:29:01 -0300

    Refactored to every page display active menu page

--- public/scripts.js
+++ public/scripts.js
@@ -49,10 +49,10 @@ function addInput(event) {
 
 document.querySelectorAll(".add-input").forEach(button => button.addEventListener("click", addInput))
 
-function isActive(element) {
-    if (element.pathname === window.location.pathname) {
-        return element.classList.add("active")
+const menuItems = document.querySelectorAll(".menu a")
+
+for (item of menuItems) {
+    if (location.pathname.includes(item.getAttribute("href"))) {
+        item.classList.add("active")
     }
 }
-
-document.querySelectorAll(".menu a").forEach(button => isActive(button))
